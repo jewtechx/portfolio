15afe1625e213c0d323bb95469f1191efc93a726
jewtechx <jklarbi_danquah@st.ug.edu.gh>
2023-08-24 02:06:58 +0000

    all

--- client/src/components/portfolio/websites/websitesData.js
+++ client/src/components/portfolio/websites/websitesData.js
@@ -11,7 +11,7 @@ const data = [
         linkToGithub: 'https://github.com/jewtechx/employees-management-system',
     },
     {
-        mainCoverImage:process.env.PUBLIC_URL + "/images/blogPortfolio/blog1.png",
+        mainCoverImage:process.env.PUBLIC_URL + "/images/blogPortfolio/blog1.PNG",
         title: "A Personal Blog Website",
         desc: "This project was created with pure html , css and js. It a fully responsive static blog website. I adhered to all design priciples and rules",
         subImages2: process.env.PUBLIC_URL + "/images/blogPortfolio/blog2.PNG",
@@ -39,7 +39,7 @@ const data = [
         subImages2: process.env.PUBLIC_URL + "/images/backerchain/backerchain2.JPG",
         subImages3: process.env.PUBLIC_URL + "/images/backerchain/backerchain3.JPG",
         subImages4: process.env.PUBLIC_URL + "/images/backerchain/backerchain4.JPG",
-        subImages5: process.env.PUBLIC_URL + "/images/backerchain/backerchain5.JPG",
+        subImages5: process.env.PUBLIC_URL + "/images/backerchain/backerChain5.JPG",
         linkToWeb: "#",
         linkToGithub: 'https://github.com/jewtechx/backerchain',
     },
